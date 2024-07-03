94243649c0beffad54815439062e1bcfa189fe5d
rip-the-slit <171191896+rip-the-slit@users.noreply.github.com>
2024-07-02 21:43:38 -0430

    Set up basic logic for the keys' event handler

--- program.js
+++ program.js
@@ -1,13 +1,13 @@
 let operand1 = "";
 let operand2 = "";
 let operator = "";
-
+ 
 const display = document.querySelector("#display")
 const keyboard = document.querySelector("#keyboard")
 const keys = [7, 8, 9, "+",
                 4, 5, 6, "-",
                 1, 2 , 3, "×",
-                "C", 0, ",", "÷",
+                "C", 0, ".", "÷",
                 "="];
 const operators = ['+', '-', '×', '÷']
 
@@ -18,31 +18,41 @@ keys.forEach((key) => {
     if (typeof key == "number") button.classList += " number";
     button.setAttribute("id", `${key}`);
     button.addEventListener("click", function (e) {
-        if (this.className.includes("number")) {
-            if (operator === "") operand1 += this.id;
-            else operand2 += this.id;
-            display.textContent += this.id;
+        if (this.className.includes("number") && operand2.length < 18) {
+            if (operand2 === "") display.textContent = "";
+            operand2 += this.id
+            display.textContent += this.id
         } else if (operators.includes(this.id)) {
-            operator += this.id
-            if (operator.length == 1 && operand2 != "") {
+            if (operand1 === "") {
+                operator = this.id
+                operand1 = operand2
+                operand2 = ""
+            } else if (operand2 != "") {
                 let result = operate(operand1, operand2, operator)
-                operand1 = "" + result
-                display.textContent = result + this.id
+                operand1 = `${result}`
+                operand2 = ""
+                operator = this.id
+                display.textContent = operand1
+            } else operator = this.id
+        } else if (this.id == "=") {
+            if (!(operand1 === "") && !(operand2 === "")) {
+                let result = operate(operand1, operand2, operator)
+                operand1 = `${result}`
+                operand2 = ""
+                display.textContent = operand1
             }
-        } else if (this.id === "=") {
-            if (operand2 === "") {
-                if (operator.length >= 1) {
-                    display.textContent = "Syntax ERROR"
-                    operand1 = ""
-                } else display.textContent = operand1;
+        } else if (this.id == "C") {
+            operand1 = operand2 = operator = ""
+            display.textContent = ""
+        } else if (this.id == ".") {
+            if (operand2 === "") display.textContent = "";
+            if (operand2 != "" && !operand2.includes("0.")) {
+                operand2 += this.id
+                display.textContent += this.id
             } else {
-                let result = operate(operand1, operand2, operator)
-                operand1 = "" + result
-                display.textContent = result
+                operand2 = "0."
+                display.textContent = operand2
             }
-            operand2 = operator = ""
-        } else if (this.id === "C") {
-            operand1 = operand2 = operator = display.textContent = "";
         }
     })
     keyboard.appendChild(button);
@@ -61,6 +71,7 @@ function multiply(operand1, operand2) {
 }
 
 function divide(operand1, operand2) {
+    if (+operand2 == 0) return "Syntax ERROR"
     return operand1 / operand2
 }
 
